0fbaba69d0570041a27a8d11bb1c5eeb25df599f
Eugen Soloviov <suenot@gmail.com>
2017-07-01 21:27:01 +0300

    auth

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
       </div>
       <div class="uk-navbar-right">
         <ul class="uk-navbar-nav">
-            <li><router-link to="/auth/facebook"><span uk-icon="icon: user"></span>&nbsp;Auth with facebook</router-link></li>
+            <li><a :href="server + '/auth/facebook'"><span uk-icon="icon: user"></span>&nbsp;Auth with facebook</a></li>
             <li><router-link to="/add"><span uk-icon="icon: happy"></span>&nbsp;Add face</router-link></li>
         </ul>
       </div>
@@ -25,8 +25,12 @@ import 'uikit/dist/css/uikit.css'
 import {$, jQuery} from 'jquery'
 import UIkit from 'uikit/dist/js/uikit.min'
 import Icons from 'uikit/dist/js/uikit-icons.min'
+import axios from 'axios'
 export default {
   name: 'app',
+  data: () => ({
+    server: 'http://74.119.194.18'
+  }),
   mounted () {
     UIkit.use(Icons)
   }
